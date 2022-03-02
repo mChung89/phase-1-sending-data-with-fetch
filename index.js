6d707b2ae905e3f0e2f3f000b94a6bf62dd24eac
Michael Chung <mikechung89@gmail.com>
2022-03-02 10:29:08 -0500

    commit tree 649863b5701dab38d540466215b15e9d4c771b92

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData (name, email) {
+    const userInfo = {
+        name: name,
+        email: email
+    };
+
+    return fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        renderInfo(data)
+    })
+    .catch(message => {
+        const h3= document.createElement(`h2`)
+        h3.textContent = message
+        document.querySelector(`body`).appendChild(h3)
+    })
+}
+
+function renderInfo(Obj) {
+    const h3 = document.createElement('h3');
+    h3.textContent = `User Name: ${Obj.name}, User Email: ${Obj.email} user id: ${Obj.id}`
+    h3.id = Obj.id
+    const body = document.querySelector('body')
+    body.append(h3)
+}
